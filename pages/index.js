cc4fdee16b6a6f1c09bcca719bc0304dc149c3c9
Muhammad Sarim <sarim@kritik.io>
2020-12-04 23:04:03 -0500

    adding new post about tiramisu and description

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>This is my blog, this is built to Next.js, click on the posts below to look at the posts</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
